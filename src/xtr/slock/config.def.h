b85e4400dfe9aa5afc9159297383665a29c99ed4
A333X <icmpkitty@grrlz.net>
2026-03-07 09:13:03 +0000

    Add files via upload

---
+++ src/xtr/slock/config.def.h
@@ -0,0 +1,56 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup"; // use "nobody" for arch
+
+static const char *colorname[NUMCOLS] = {
+	[BACKGROUND] =  "#2d2d2d", /* after initialization */
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+	[CAPS] =   "red",       /* CapsLock on */
+};
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x10";
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 75;
+static const int logow = 12;   /* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[] = {
+   /* x    y   w   h */
+   { 0,    3,  1,  3 },
+   { 1,    3,  2,  1 },
+   { 0,    5,  8,  1 },
+   { 3,    0,  1,  5 },
+   { 5,    3,  1,  2 },
+   { 7,    3,  1,  2 },
+   { 8,    3,  4,  1 },
+   { 9,    4,  1,  2 },
+   { 11,   4,  1,  2 },
+};
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "background",   STRING,  &colorname[BACKGROUND] },
+		{ "locked",       STRING,  &colorname[INIT] },
+		{ "input",        STRING,  &colorname[INPUT] },
+		{ "failed",       STRING,  &colorname[FAILED] },
+		{ "capslock",     STRING,  &colorname[CAPS] },
+		{ "message",      STRING,  &message },
+		{ "text_color",   STRING,  &text_color },
+		{ "font_name",    STRING,  &font_name },
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
